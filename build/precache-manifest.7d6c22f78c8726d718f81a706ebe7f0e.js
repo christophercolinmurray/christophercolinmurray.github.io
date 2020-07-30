7c62aa6097a3672bde6ae9d2d4892bf1fdc962c1
Christopher Murray <christopher@christopher-murray.com>
2020-07-30 13:05:02 -0400

    Fix: not indexing build directory. Remove gitignore /build

---
+++ build/precache-manifest.7d6c22f78c8726d718f81a706ebe7f0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2b97f85a9d64440e1ac31a87d563fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b60dbcbb5a49286ba50",
+    "url": "/static/css/main.f775c022.chunk.css"
+  },
+  {
+    "revision": "8ef774d1f4a7bcb2b505",
+    "url": "/static/js/2.ebeb2c73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ebeb2c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b60dbcbb5a49286ba50",
+    "url": "/static/js/main.70fd448f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
