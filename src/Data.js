98c357db5d42ae638f33e4fc9306b29019daae69
Christopher Murray <christopher@christopher-murray.com>
2020-08-01 10:58:02 -0400

    🌟 Update links

--- src/Data.js
+++ src/Data.js
@@ -23,7 +23,7 @@ let Data = {
       },
       {
         "name":"Google Drive Resume",
-        "url":"https://drive.google.com/file/d/1ac5g4Ens91zUnyRtcNi9CGs4TMlEwYoo/view?usp=sharing",
+        "url":"https://drive.google.com/file/d/1a1lfk70F6lYm3ohBhexyVIOCxdhlwozE/view",
         "d":"M4.433 22.396l4-6.929H24l-4 6.929H4.433zm3.566-6.929l-3.998 6.929L0 15.467 7.785 1.98l3.999 6.931-3.785 6.556zm15.784-.375h-7.999L7.999 1.605h8.002l7.785 13.486h-.003z"
       }
     ],
